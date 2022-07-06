60083648fac089a7420df6e77c2a43bf3196cf7f
Kiran Bansode <kiran5120135@gmail.com>
2022-07-06 13:46:43 +0530

    02.04. How We Use TypeScript

---
+++ 02/basics.js
@@ -0,0 +1,4 @@
+console.log("Hello!");
+console.log("Goodbye!!!");
+console.log(Math.round(7.3287));
+var pi = 3.147;
---
+++ 02/basics.ts
@@ -0,0 +1,5 @@
+console.log("Hello!");
+console.log("Goodbye!!!");
+console.log(Math.round(7.3287));
+
+let pi: number = 3.147;
