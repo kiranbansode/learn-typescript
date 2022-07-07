2c25b8be428149ab895cd0fbad25e9f3e1938291
Kiran Bansode <kiran5120135@gmail.com>
2022-07-07 22:07:44 +0530

    03.02. Working With Numbers & Booleans

--- 03/variables.ts
+++ 03/variables.ts
@@ -2,3 +2,11 @@ let movieTitle: string = "Amadeus";
 movieTitle = "Arrival";
 movieTitle = 9; // This will give an error
 movieTitle.toUpperCase();
+
+let numCatLives: number = 9;
+numCatLives += 1;
+// numCatLives = "zero";
+
+let gameOver: boolean = false;
+gameOver = true;
+gameOver = "true"; // This will give an error
