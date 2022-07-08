48c7c512f91e4c30c85621aedbcb8110b9e8e69a
Kiran Bansode <kiran5120135@gmail.com>
2022-07-08 13:23:52 +0530

    04.04. Working With Default Parameters

--- 04/functions.ts
+++ 04/functions.ts
@@ -2,12 +2,16 @@ function square(num: number) {
 	return num * num;
 }
 
-function greet(person: string) {
-	return `Hi there, ${person}`;
-}
+// function greet(person: string) {
+// 	return `Hi there, ${person}`;
+// }
 
 const doSomething = (person: string, age: number, isFunny: boolean) => {};
 
 square(3);
 greet("Tonya Harding");
 doSomething("Chicken", 78, true);
+
+function greet(person: string = "stranger") {
+	return `Hi there, ${person}`;
+}
