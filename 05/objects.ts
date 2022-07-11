ce926fc7bb18e3ffe6602fc7f392ff536898e808
Kiran Bansode <kiran5120135@gmail.com>
2022-07-11 13:06:08 +0530

    05.02. More Object Types

--- 05/objects.ts
+++ 05/objects.ts
@@ -3,3 +3,9 @@ function printName(person: { first: string; last: string }): void {
 }
 
 printName({ first: 'thomas', last: 'Jenkins' });
+
+let coordinate: { x: number; y: number } = { x: 32, y: 2 };
+
+function randomCoordinate(): { x: number; y: number } {
+	return { x: Math.random(), y: Math.random() };
+}
