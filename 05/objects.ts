fac7a30cccbe6ee64bba4c55cd4d2a63aa0fb594
Kiran Bansode <kiran5120135@gmail.com>
2022-07-11 13:16:52 +0530

    05.05. Creating Type Aliases

--- 05/objects.ts
+++ 05/objects.ts
@@ -1,16 +1,34 @@
-function printName(person: { first: string; last: string }): void {
-	console.log(`${person.first} ${person.last}`);
-}
+// function printName(person: { first: string; last: string }): void {
+// 	console.log(`${person.first} ${person.last}`);
+// }
 
-printName({ first: 'thomas', last: 'Jenkins' });
+// printName({ first: 'thomas', last: 'Jenkins' });
 
-let coordinate: { x: number; y: number } = { x: 32, y: 2 };
+// let coordinate: { x: number; y: number } = { x: 32, y: 2 };
 
-function randomCoordinate(): { x: number; y: number } {
+// function randomCoordinate(): { x: number; y: number } {
+// 	return { x: Math.random(), y: Math.random() };
+// }
+
+// printName({ first: 'Mick', last: 'Jagger', age: 473 });
+
+// const singer = { first: 'Mick', last: 'Jagger', age: 473, isAlive: true };
+// printName(singer);
+
+type Point = {
+	x: number;
+	y: number;
+};
+
+let coordinate: Point = { x: 32, y: 2 };
+
+function randomCoordinate(): Point {
 	return { x: Math.random(), y: Math.random() };
 }
 
-printName({ first: 'Mick', last: 'Jagger', age: 473 });
+function doublePOint(point: Point): Point {
+	return { x: point.x * 2, y: point.y * 2 };
+}
 
-const singer = { first: 'Mick', last: 'Jagger', age: 473, isAlive: true };
-printName(singer);
+type MyNum = number;
+let age: MyNum = 24234;
