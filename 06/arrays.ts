6f696a87a5669dde2f8fc7f8e18dd7e25ad1a0d9
Kiran Bansode <kiran5120135@gmail.com>
2022-07-12 15:21:46 +0530

    06.02. More Array Syntax

--- 06/arrays.ts
+++ 06/arrays.ts
@@ -4,3 +4,16 @@ activeUsers.push('kiran');
 const ageList: number[] = [45, 56, 13];
 ageList[0] = 99;
 ageList[0] = 'not a number';
+
+// const booleans: Array<boolean> = [true, false];
+const booleans: boolean[] = [];
+
+type Point = {
+	x: number;
+	y: number;
+};
+
+const coords: Point[] = [];
+
+coords.push({ x: 23, y: 8 });
+coords.push({ x: 23, y: "8" });
