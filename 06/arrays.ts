e1597424b434185b14f035f78913be8fde36c5fa
Kiran Bansode <kiran5120135@gmail.com>
2022-07-12 15:25:15 +0530

    06.03. Multidimensional Arrays

--- 06/arrays.ts
+++ 06/arrays.ts
@@ -3,7 +3,7 @@ activeUsers.push('kiran');
 
 const ageList: number[] = [45, 56, 13];
 ageList[0] = 99;
-ageList[0] = 'not a number';
+// ageList[0] = 'not a number';
 
 // const booleans: Array<boolean> = [true, false];
 const booleans: boolean[] = [];
@@ -16,4 +16,12 @@ type Point = {
 const coords: Point[] = [];
 
 coords.push({ x: 23, y: 8 });
-coords.push({ x: 23, y: "8" });
+// coords.push({ x: 23, y: "8" });
+
+const board: string[][] = [
+	['X', 'O', 'X'],
+	['X', 'O', 'X'],
+	['X', 'O', 'X'],
+];
+
+const demo: number[][][] = [[[1]]];
