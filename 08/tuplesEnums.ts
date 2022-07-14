f32b50b02ab2d6fa757f9e5ffe6fbfd848028d93
Kiran Bansode <kiran5120135@gmail.com>
2022-07-14 19:40:03 +0530

    08.02. A Bit More On Tuples

--- 08/tuplesEnums.ts
+++ 08/tuplesEnums.ts
@@ -1,4 +1,18 @@
-const stuff: (string | number)[] = [1, 'asd', 'asdasd', 'asdasd', 2];
+// const stuff: (string | number)[] = [1, 'asd', 2];
 
 // const color: number[] = [23, 45, 234, 234];
 const color: [number, number, number] = [255, 0, 45];
+
+type HTTPResponse = [number, string];
+
+const goodRes: HTTPResponse = [200, 'ok'];
+// goodRes[0] = '200'; // This will give an Error
+// goodRes.push(123);
+// goodRes.pop();
+// goodRes.pop();
+// goodRes.pop();
+
+const responses: HTTPResponse[] = [
+	[404, 'Not Found'],
+	[200, 'OK'],
+];
