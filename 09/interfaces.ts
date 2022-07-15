6651bad5ea3961016f1ae3fffd9ec7224fd6c2e1
Kiran Bansode <kiran5120135@gmail.com>
2022-07-15 17:25:32 +0530

    09.07. Extending Interfaces

--- 09/interfaces.ts
+++ 09/interfaces.ts
@@ -70,3 +70,17 @@ const elton: Dog = {
 		return 'Woof Woof!';
 	},
 };
+
+interface ServiceDog extends Dog {
+	job: 'drug sniffer' | 'bomb' | 'guide dog';
+}
+
+const chewy: ServiceDog = {
+	name: 'Chewy',
+	age: 4.5,
+	breed: ' Lab',
+	bark() {
+		return 'Bark';
+	},
+	job: 'guide dog',
+};
