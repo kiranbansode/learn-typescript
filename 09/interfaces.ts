f82362b9fc459bf46afa0b2db6ce800f53f44279
Kiran Bansode <kiran5120135@gmail.com>
2022-07-15 17:06:57 +0530

    09.02. Readonly and Optional Interface Properties

--- 09/interfaces.ts
+++ 09/interfaces.ts
@@ -11,3 +11,20 @@ interface Point {
 }
 
 const pt: Point = { x: 123, y: 1234 };
+
+interface Person {
+	readonly id: number;
+	first: string;
+	last: string;
+	nickname?: string;
+}
+
+const thomas: Person = {
+	id: 21837,
+	first: 'Thomas',
+	last: 'Hardy',
+	// nickname: 'Hardy',
+};
+
+thomas.first = 'Specter';
+// thomas.id = 123; // NOT Allowed
