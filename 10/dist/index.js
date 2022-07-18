3727c4067377265155749a69f611173885292007
Kiran Bansode <kiran5120135@gmail.com>
2022-07-18 20:43:50 +0530

    10.06. Outdir Option

---
+++ 10/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const norma = {
+    breed: 'Silky',
+    eggsPerWeek: 4,
+    name: 'Norma',
+    age: 2,
+};
+const juniper = {
+    breed: 'Silky',
+    eggsPerWeek: 4,
+    name: 'Juniper',
+    age: 1,
+};
