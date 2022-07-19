0aa59583c44a3345a6697957b9842f2c50d9f69a
Kiran Bansode <kiran5120135@gmail.com>
2022-07-19 13:28:20 +0530

    11.08. Building The Todo List

--- 11/dist/index.js
+++ 11/dist/index.js
@@ -2,9 +2,18 @@
 var btn = document.getElementById('btn');
 var input = document.getElementById('todo-input');
 var form = document.querySelector('form');
+var list = document.getElementById('todo-list');
 var handleSubmit = function (event) {
     event.preventDefault();
-    console.log('Submit');
+    var newTodoText = input.value;
+    var newLI = document.createElement('li');
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newLI.append(newTodoText);
+    newLI.append(checkbox);
+    list === null || list === void 0 ? void 0 : list.append(newLI);
+    input.value = '';
+    input.focus();
 };
 form.addEventListener('submit', handleSubmit);
 // btn.addEventListener('click', () => {
--- 11/src/index.ts
+++ 11/src/index.ts
@@ -1,10 +1,21 @@
 const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todo-input')! as HTMLInputElement;
 const form = document.querySelector('form')!;
+const list = document.getElementById('todo-list')!;
 
 const handleSubmit = (event: SubmitEvent) => {
 	event.preventDefault();
-	console.log('Submit');
+	const newTodoText = input.value;
+	const newLI = document.createElement('li');
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+
+	newLI.append(newTodoText);
+	newLI.append(checkbox);
+
+	list?.append(newLI);
+	input.value = '';
+	input.focus();
 };
 
 form.addEventListener('submit', handleSubmit);
