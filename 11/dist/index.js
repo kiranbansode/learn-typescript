a1cfa5328800bb7b5f00c9cba735f394e844b712
Kiran Bansode <kiran5120135@gmail.com>
2022-07-19 13:20:53 +0530

    11.07. Working With Events

--- 11/dist/index.js
+++ 11/dist/index.js
@@ -1,7 +1,13 @@
 "use strict";
 var btn = document.getElementById('btn');
 var input = document.getElementById('todo-input');
-btn.addEventListener('click', function () {
-    alert(input.value);
-    input.value = '';
-});
+var form = document.querySelector('form');
+var handleSubmit = function (event) {
+    event.preventDefault();
+    console.log('Submit');
+};
+form.addEventListener('submit', handleSubmit);
+// btn.addEventListener('click', () => {
+// 	alert(input.value);
+// 	input.value = '';
+// });
--- 11/src/index.ts
+++ 11/src/index.ts
@@ -1,7 +1,15 @@
 const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todo-input')! as HTMLInputElement;
+const form = document.querySelector('form')!;
 
-btn.addEventListener('click', () => {
-	alert(input.value);
-	input.value = '';
-});
+const handleSubmit = (event: SubmitEvent) => {
+	event.preventDefault();
+	console.log('Submit');
+};
+
+form.addEventListener('submit', handleSubmit);
+
+// btn.addEventListener('click', () => {
+// 	alert(input.value);
+// 	input.value = '';
+// });
