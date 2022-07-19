4cb093dfb5f76c92370f2fdfd1d689e93f0ee9bb
Kiran Bansode <kiran5120135@gmail.com>
2022-07-19 11:04:10 +0530

    11.02. Working With The DOM

--- 11/dist/index.js
+++ 11/dist/index.js
@@ -1,6 +1,3 @@
 "use strict";
-function printDouble(msg) {
-    console.log(msg);
-    console.log(msg);
-}
-printDouble('Hello World :');
+const btn = document.getElementById('btn');
+console.log(btn);
--- 11/src/index.ts
+++ 11/src/index.ts
@@ -1,6 +1,2 @@
-function printDouble(msg: string) {
-	console.log(msg);
-	console.log(msg);
-}
-
-printDouble('Hello World :');
+const btn = document.getElementById('btn');
+console.log(btn);
