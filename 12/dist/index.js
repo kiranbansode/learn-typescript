c3e2c1121afaed466d00d9164840d06ec6dc4d77
Kiran Bansode <kiran5120135@gmail.com>
2022-07-20 21:10:27 +0530

    12.04. Class Fields

--- 12/dist/index.js
+++ 12/dist/index.js
@@ -1,4 +1,7 @@
 class Player {
+	score = 0;
+	numLives = 10;
+
 	constructor(first, last) {
 		this.first = first;
 		this.last = last;
@@ -7,13 +10,20 @@ class Player {
 	taunt() {
 		console.log('HOORAH!');
 	}
+
+	loseLife() {
+		this.numLives -= 1;
+	}
 }
 
 const player1 = new Player('blue', 'steel');
-player1.taunt();
-console.log(player1.first);
-console.log(player1.last);
+// player1.taunt();
+// console.log(player1.first);
+// console.log(player1.last);
+console.log(player1);
+console.log(player1.loseLife());
+console.log(player1);
 
-const player2 = new Player('charlie', 'brown');
-player2.taunt();
-console.log(player2);
+// const player2 = new Player('charlie', 'brown');
+// player2.taunt();
+// console.log(player2);
