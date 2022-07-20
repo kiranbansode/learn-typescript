63d14ff3ca3ea24a06248d5d8315d9dddc1fc9c5
Kiran Bansode <kiran5120135@gmail.com>
2022-07-20 21:01:30 +0530

    12.02. The Class Keyword

--- 12/dist/index.js
+++ 12/dist/index.js
@@ -1,2 +1,11 @@
-"use strict";
-console.log('Hello');
+class Player {
+	taunt() {
+		console.log('HOORAH!');
+	}
+}
+
+const player1 = new Player();
+player1.taunt();
+
+const player2 = new Player();
+player2.taunt();
