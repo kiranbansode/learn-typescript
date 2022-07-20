f74b43b00c8705828bcf7761ddf06c330d687a24
Kiran Bansode <kiran5120135@gmail.com>
2022-07-20 21:05:25 +0530

    12.03. Constructors

--- 12/dist/index.js
+++ 12/dist/index.js
@@ -1,11 +1,19 @@
 class Player {
+	constructor(first, last) {
+		this.first = first;
+		this.last = last;
+	}
+
 	taunt() {
 		console.log('HOORAH!');
 	}
 }
 
-const player1 = new Player();
+const player1 = new Player('blue', 'steel');
 player1.taunt();
+console.log(player1.first);
+console.log(player1.last);
 
-const player2 = new Player();
+const player2 = new Player('charlie', 'brown');
 player2.taunt();
+console.log(player2);
