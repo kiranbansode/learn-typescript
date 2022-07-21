e0b77aac121dbb6b1fb1fd2b815c6da092575340
Kiran Bansode <kiran5120135@gmail.com>
2022-07-21 13:35:45 +0530

    13.06. Parameter Properties Shorthand

--- 13/dist/index.js
+++ 13/dist/index.js
@@ -1,16 +1,28 @@
 "use strict";
+// class Player {
+// 	public readonly first: string;
+// 	public readonly last: string;
+// 	private score = 0;
+// 	constructor(first: string, last: string) {
+// 		this.first = first;
+// 		this.last = last;
+// 		// this.secretMethod();
+// 	}
+// 	private secretMethod(): void {
+// 		console.log('Secret Method');
+// 	}
+// }
+// elton.score = 42;
+// elton.first = 'Kiran';
+// elton.secretMethod();
 class Player {
-    constructor(first, last) {
-        this.score = 0;
+    constructor(first, last, score) {
         this.first = first;
         this.last = last;
-        // this.secretMethod();
+        this.score = score;
     }
     secretMethod() {
         console.log('Secret Method');
     }
 }
-const elton = new Player('Elton', 'Steele');
-// elton.score = 42;
-// elton.first = 'Kiran';
-// elton.secretMethod();
+const elton = new Player('Elton', 'Steele', 100);
--- 13/src/index.ts
+++ 13/src/index.ts
@@ -1,20 +1,33 @@
-class Player {
-	public readonly first: string;
-	public readonly last: string;
-	private score = 0;
+// class Player {
+// 	public readonly first: string;
+// 	public readonly last: string;
+// 	private score = 0;
 
-	constructor(first: string, last: string) {
-		this.first = first;
-		this.last = last;
-		// this.secretMethod();
-	}
+// 	constructor(first: string, last: string) {
+// 		this.first = first;
+// 		this.last = last;
+// 		// this.secretMethod();
+// 	}
+
+// 	private secretMethod(): void {
+// 		console.log('Secret Method');
+// 	}
+// }
+
+// elton.score = 42;
+// elton.first = 'Kiran';
+// elton.secretMethod();
+
+class Player {
+	constructor(
+		public first: string,
+		public last: string,
+		private score: number
+	) {}
 
 	private secretMethod(): void {
 		console.log('Secret Method');
 	}
 }
 
-const elton = new Player('Elton', 'Steele');
-// elton.score = 42;
-// elton.first = 'Kiran';
-// elton.secretMethod();
+const elton = new Player('Elton', 'Steele', 100);
