3fb7ce1bf4aaf68e38bd4bca066c42c6658d8cb5
Kiran Bansode <kiran5120135@gmail.com>
2022-07-22 20:49:52 +0530

    14.03. Writing Our First Generic

--- 14/dist/index.js
+++ 14/dist/index.js
@@ -1,8 +1,23 @@
 "use strict";
 // const nums: number[] = []
-var nums = [];
-var colors = [];
-var inputEl = document.querySelector('#username');
-console.log(inputEl);
-inputEl.value = 'Hacked!';
-var btn = document.querySelector('.btn');
+// const nums: Array<number> = [];
+// const colors: Array<string> = [];
+// const inputEl = document.querySelector<HTMLInputElement>('#username')!;
+// console.log(inputEl);
+// inputEl.value = 'Hacked!';
+// const btn = document.querySelector<HTMLButtonElement>('.btn')!;
+function numberIdentity(item) {
+    return item;
+}
+function stringIdentity(item) {
+    return item;
+}
+function booleanIdentity(item) {
+    return item;
+}
+function identity(item) {
+    return item;
+}
+// identity<number>(7);
+// identity<string>('hello');
+// identity<Cat>({ name: 'Rocky', breed: 'African' });
--- 14/src/index.ts
+++ 14/src/index.ts
@@ -1,9 +1,38 @@
 // const nums: number[] = []
-const nums: Array<number> = [];
-const colors: Array<string> = [];
+// const nums: Array<number> = [];
+// const colors: Array<string> = [];
 
-const inputEl = document.querySelector<HTMLInputElement>('#username')!;
-console.log(inputEl);
-inputEl.value = 'Hacked!';
+// const inputEl = document.querySelector<HTMLInputElement>('#username')!;
+// console.log(inputEl);
+// inputEl.value = 'Hacked!';
 
-const btn = document.querySelector<HTMLButtonElement>('.btn')!;
+// const btn = document.querySelector<HTMLButtonElement>('.btn')!;
+
+function numberIdentity(item: number): number {
+	return item;
+}
+
+function stringIdentity(item: number): number {
+	return item;
+}
+
+function booleanIdentity(item: boolean): boolean {
+	return item;
+}
+
+// function identity(item: any): any {
+// 	return item;
+// }
+
+interface Cat {
+	name: string;
+	breed: string;
+}
+
+function identity<T>(item: T): T {
+	return item;
+}
+
+// identity<number>(7);
+// identity<string>('hello');
+// identity<Cat>({ name: 'Rocky', breed: 'African' });
