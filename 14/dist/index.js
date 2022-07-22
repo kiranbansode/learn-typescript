eea1a1061b663200dacc1e62d8d1272a366b0e74
Kiran Bansode <kiran5120135@gmail.com>
2022-07-22 20:41:41 +0530

    14.02. Another Example Of A Built-In Generic

---
+++ 14/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// const nums: number[] = []
+var nums = [];
+var colors = [];
+var inputEl = document.querySelector('#username');
+console.log(inputEl);
+inputEl.value = 'Hacked!';
+var btn = document.querySelector('.btn');
