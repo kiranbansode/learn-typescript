8b5f2cfdbdb7d91e1788dab1c5ef5f11a55d2b5b
kiranbansode <kiran5120135@gmail.com>
2022-07-28 22:05:21 +0530

    2.10. Working with Tuples

--- 02/app.js
+++ 02/app.js
@@ -5,12 +5,17 @@
 var person = {
     name: "Kiran",
     age: 26,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
+// person.role.push("admin"); // push() is exception
+// person.role[1] = 10;
+// person.role = [0, "admin", "user"];
 var favouriteActivities;
 favouriteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // ERROR
 }
--- 02/app.ts
+++ 02/app.ts
@@ -2,11 +2,21 @@
 // 	name: string;
 // 	age: number;
 // } = {
-const person = {
+const person: {
+	name: string;
+	age: number;
+	hobbies: string[];
+	role: [number, string]; // Tuple
+} = {
 	name: "Kiran",
 	age: 26,
 	hobbies: ["Sports", "Cooking"],
+	role: [2, "author"],
 };
+
+// person.role.push("admin"); // push() is exception
+// person.role[1] = 10;
+// person.role = [0, "admin", "user"];
 
 let favouriteActivities: string[];
 favouriteActivities = ["Sports"];
