102d858043eac75473394c0a2cc199e1634bfe77
kiranbansode <kiran5120135@gmail.com>
2022-07-28 21:56:26 +0530

    2.09. Arrays Types

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,16 @@
+// const person: {
+// 	name: string;
+// 	age: number;
+// } = {
 var person = {
     name: "Kiran",
-    age: 26
+    age: 26,
+    hobbies: ["Sports", "Cooking"]
 };
+var favouriteActivities;
+favouriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- 02/app.ts
+++ 02/app.ts
@@ -5,6 +5,15 @@
 const person = {
 	name: "Kiran",
 	age: 26,
+	hobbies: ["Sports", "Cooking"],
 };
 
+let favouriteActivities: string[];
+favouriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+	console.log(hobby.toUpperCase());
+	// console.log(hobby.map()); // ERROR
+}
