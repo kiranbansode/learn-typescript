59df40440ee774c3d27a981630c741e0520e456d
kiranbansode <kiran5120135@gmail.com>
2022-07-28 22:19:53 +0530

    2.11. Working with Enums

--- 02/app.ts
+++ 02/app.ts
@@ -2,16 +2,33 @@
 // 	name: string;
 // 	age: number;
 // } = {
-const person: {
-	name: string;
-	age: number;
-	hobbies: string[];
-	role: [number, string]; // Tuple
-} = {
+// const person: {
+// 	name: string;
+// 	age: number;
+// 	hobbies: string[];
+// 	role: [number, string]; // Tuple
+// } = {
+// 	name: "Kiran",
+// 	age: 26,
+// 	hobbies: ["Sports", "Cooking"],
+// 	role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+	ADMIN = "ADMIN",
+	READ_ONLY = 100,
+	AUTHOR, // 3 - automatically assigned
+}
+
+const person = {
 	name: "Kiran",
 	age: 26,
 	hobbies: ["Sports", "Cooking"],
-	role: [2, "author"],
+	role: Role.ADMIN,
 };
 
 // person.role.push("admin"); // push() is exception
@@ -26,4 +43,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
 	console.log(hobby.toUpperCase());
 	// console.log(hobby.map()); // ERROR
+}
+
+if (person.role === Role.AUTHOR) {
+	console.log("is author");
 }
